665a794e4aa871781f1fba3f4dc6378a878c5e18
William David Vasquez Parada <william.vasquez@ci2.co>
2022-08-18 09:48:34 -0500

    cambio de nombre contrato

--- ci2token.sol
+++ ci2token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("Ci2Token", "CI2T") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
